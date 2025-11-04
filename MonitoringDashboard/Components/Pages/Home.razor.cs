c87cc3a21717135b5917e8302005528fab9a36bb
Jakub Sokol <sokoljakub14@gmail.com>
2025-11-05 00:08:31 +0100

    fix: use new db context for SignalR in Home page

--- MonitoringDashboard/Components/Pages/Home.razor.cs
+++ MonitoringDashboard/Components/Pages/Home.razor.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.JSInterop;
+using MonitoringDashboard.Data;
 using MonitoringDashboard.Data.Models;
 
 namespace MonitoringDashboard.Components.Pages;
@@ -39,8 +40,13 @@ public partial class Home : IAsyncDisposable
 
     private async Task UpdateMonitoredServices()
     {
-        _monitoredServices = await Db.MonitoredServices.OrderByDescending(m => m.Name)
+        using var scope = ScopeFactory.CreateScope();
+        var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        _monitoredServices = await db.MonitoredServices
+            .OrderByDescending(m => m.Name)
             .Include(m => m.Checks.OrderByDescending(c => c.CheckedAt).Take(5))
+            .AsNoTracking()
             .ToListAsync();
     }
     
