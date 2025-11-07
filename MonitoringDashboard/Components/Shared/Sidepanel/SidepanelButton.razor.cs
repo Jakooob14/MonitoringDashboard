d93e8bbfb4726494ff3a2bc256a2fad56f86ec29
Jakub Sokol <sokoljakub14@gmail.com>
2025-11-07 19:50:51 +0100

    feat: add navigation and activation functionality to SidepanelButton

--- MonitoringDashboard/Components/Shared/Sidepanel/SidepanelButton.razor.cs
+++ MonitoringDashboard/Components/Shared/Sidepanel/SidepanelButton.razor.cs
@@ -1,9 +1,41 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
 
 namespace MonitoringDashboard.Components.Shared.Sidepanel;
 
-public partial class SidepanelButton : ComponentBase
+public partial class SidepanelButton : ComponentBase, IDisposable
 {
     [Parameter] public required string Text { get; set; } = string.Empty;
     [Parameter] public string IconPath { get; set; } = string.Empty;
+    [Parameter] public string Href { get; set; } = "#";
+        
+    private EventHandler<LocationChangedEventArgs>? _locationChangedHandler;
+
+    public void Dispose()
+    {
+        if (_locationChangedHandler != null) Nav.LocationChanged -= _locationChangedHandler;
+    }
+    
+    protected override void OnInitialized()
+    {
+        _locationChangedHandler = (_, __) => StateHasChanged();
+        Nav.LocationChanged += _locationChangedHandler;
+    }
+    
+    private string GetClass()
+    {
+        var uri = Nav.Uri;
+        var baseUri = Nav.BaseUri;
+        var relativePath = uri.Replace(baseUri, "", StringComparison.OrdinalIgnoreCase);
+
+        // Active if it starts with Href
+        bool isActive = relativePath.StartsWith(Href.TrimStart('/'), StringComparison.OrdinalIgnoreCase);
+
+        return isActive ? "bg-zinc-900" : "";
+    }
+    
+    private void OnClick()
+    {
+        Nav.NavigateTo(Href);
+    }
 }
